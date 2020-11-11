983c2f3c71561cbb3b744c4d11f2f7efbca1ad2b
Tony Odom <todom86@gmail.com>
2020-11-10 21:47:10 -0700

    salary histogram on python completed

--- EmployeeSQL/employee_queries.sql
+++ EmployeeSQL/employee_queries.sql
@@ -1,15 +1,18 @@
+-- Salaries of each employee
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees e
 JOIN salaries s
 ON e.emp_no = s.emp_no
 ;
 
+-- Every employee hired in 1986
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE
 EXTRACT(YEAR FROM hire_date) = 1986
 ;
 
+-- Department Managers
 SELECT d.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
 FROM departments d
 JOIN dept_manager dm
@@ -18,20 +21,24 @@ ON d.dept_no = dm.dept_no
 	ON dm.emp_no = e.emp_no
 ;
 
+-- Departments each employee worked in
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees e
 JOIN dept_emp de
 ON e.emp_no = de.emp_no
 	JOIN departments d
 	ON de.dept_no = d.dept_no
+	ORDER BY e.last_name
 ;
 
+-- Hercules B.
 SELECT first_name, last_name, sex
 FROM employees
 WHERE first_name = 'Hercules'
 AND last_name LIKE 'B%'
 ;
 
+-- Sales Employees
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees e
 JOIN dept_emp de
@@ -41,6 +48,7 @@ ON e.emp_no = de.emp_no
 	WHERE d.dept_name = 'Sales'
 ;
 
+-- Sales & Development Employees
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees e
 JOIN dept_emp de
@@ -51,6 +59,7 @@ ON e.emp_no = de.emp_no
 	OR d.dept_name ='Development'
 ;
 
+-- Count of last names
 SELECT last_name, COUNT(last_name) AS count
 FROM employees
 GROUP BY last_name
